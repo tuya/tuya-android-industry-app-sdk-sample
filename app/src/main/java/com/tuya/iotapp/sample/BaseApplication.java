ad451f792fe53e4f0cf18131e6b3534edbcabb5b
乾启 <sunrw@tuya.com>
2021-03-24 16:54:31 +0800

    [add] full body sign [remove] useless login module

---
+++ app/src/main/java/com/tuya/iotapp/sample/BaseApplication.java
@@ -0,0 +1,32 @@
+package com.tuya.iotapp.sample;
+
+import android.app.Application;
+
+import com.getkeepsafe.relinker.ReLinker;
+import com.tuya.iotapp.network.IotAppNetWork;
+import com.tuya.iotapp.network.api.IApiUrlProvider;
+import com.tuya.iotapp.sample.env.EnvUrlProvider;
+import com.tuya.iotapp.sample.env.EnvUtils;
+
+/**
+ * Base Application
+ *
+ * @author 乾启 <a href="mailto:sunrw@tuya.com">Contact me.</a>
+ * @since 2021/3/24 11:05 AM
+ */
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ReLinker.loadLibrary(this, "tuya_sign");
+
+        EnvUtils.setEnv(this, EnvUtils.ENV_PRE); //环境区分
+        IApiUrlProvider provider = new EnvUrlProvider(this);
+        IotAppNetWork.initialize(getApplicationContext(),
+                "79pc3encafyeqns0qv4j",
+                "03f425b24129446eadadb053e4abde48",
+                "Android",
+                provider);
+    }
+}
