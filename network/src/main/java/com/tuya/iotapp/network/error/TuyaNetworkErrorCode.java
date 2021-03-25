ba1638b65cbbc0109f00f36a6b278159838bec23
乾启 <sunrw@tuya.com>
2021-03-25 14:57:28 +0800

    [add] AccessTokenManager clearInfo

---
+++ network/src/main/java/com/tuya/iotapp/network/error/TuyaNetworkErrorCode.java
@@ -0,0 +1,23 @@
+package com.tuya.iotapp.network.error;
+
+/**
+ * Network error code
+ *
+ * @author 乾启 <a href="mailto:sunrw@tuya.com">Contact me.</a>
+ * @since 2021/3/25 11:48 AM
+ */
+interface TuyaNetworkErrorCode {
+    int SYSTEM_ERROR = 500;
+
+    int INVALID_SING = 1004;
+
+    int TOKEN_INVALID = 1010;
+
+    int PARAM_ERROR = 1100;
+
+    int PERMISSION_DENY = 1106;
+
+    int PARAM_ILLEGAL = 1109;
+
+
+}
